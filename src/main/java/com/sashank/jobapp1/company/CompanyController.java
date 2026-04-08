e04b9992487499cdab3e747c3521784fc382ec75
sashank solasa <sashanksolasa@gmail.com>
2026-04-09 01:47:04 +0530

    Update Companies

--- src/main/java/com/sashank/jobapp1/company/CompanyController.java
+++ src/main/java/com/sashank/jobapp1/company/CompanyController.java
@@ -1,8 +1,8 @@
 package com.sashank.jobapp1.company;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +17,10 @@ public class CompanyController {
     @GetMapping
     public List<Company> getAllCompanies(){
         return companyService.getAllCompanies();
+    }
+    @PutMapping({"/{id"})
+    public ResponseEntity<String > updateCompany(@PathVariable Long id, @RequestBody Company company){
+        companyService.updateCompany(company,id);
+        return new ResponseEntity<>("Company Updated Sucessfully", HttpStatus.OK);
     }
 }
--- src/main/java/com/sashank/jobapp1/company/impl/CompanyServiceImpl.java
+++ src/main/java/com/sashank/jobapp1/company/impl/CompanyServiceImpl.java
@@ -3,9 +3,12 @@ package com.sashank.jobapp1.company.impl;
 import com.sashank.jobapp1.company.Company;
 import com.sashank.jobapp1.company.CompanyRepository;
 import com.sashank.jobapp1.company.CompanyService;
+import com.sashank.jobapp1.job.Job;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CompanyServiceImpl implements CompanyService {
     private  CompanyRepository companyRepository;
@@ -18,4 +21,21 @@ public class CompanyServiceImpl implements CompanyService {
     public List<Company> getAllCompanies() {
         return companyRepository.findAll();
     }
+
+    @Override
+    public boolean updateCompany(Company company,Long id) {
+        Optional<Company> companyOptional =companyRepository.findById(id);
+        if(companyOptional.isPresent()){
+            Company companyToUpdate= companyOptional.get();
+
+            companyToUpdate.setDescription(companyToUpdate.getDescription());
+            companyToUpdate.setName(companyToUpdate.getName());
+            companyToUpdate.setJobs(companyToUpdate.getJobs());
+            companyRepository.save(companyToUpdate);
+            return true;
+
+        }
+        return false;
+    }
+
 }
